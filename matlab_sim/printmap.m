0df88dc4805005898199ab2b498b82a97fb27533
bangrock <keshavr@uw.edu>
2016-11-17 17:57:14 -0800

    Update printmap.m

--- matlab_sim/printmap.m
+++ matlab_sim/printmap.m
@@ -1,8 +1,10 @@
 function printmap(M,scale)
 for i = 1:size(M,1)
     for j = 1:size(M,2)
-        rectangle('Position',[scale*i scale*j scale scale],'FaceColor',M(i,j)*ones(1,3),'LineStyle','none')
-        hold on
+       if(M(i,j)!=1)
+         rectangle('Position',[scale*i scale*j scale scale],'FaceColor',M(i,j)*ones(1,3),'LineStyle','none')
+         hold on
+       end
     end
 end
 axis([0 scale*size(M,1) 0 scale*size(M,2)]);
